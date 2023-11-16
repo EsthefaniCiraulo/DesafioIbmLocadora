70ba2a92e46f7dfa9f34d6854a3617084dd4831f
EsthefaniCiraulo <133721152+EsthefaniCiraulo@users.noreply.github.com>
2023-11-16 20:23:53 -0300

    Update usuarioMock.ts

--- frontend/src/app/db/usuarioMock.ts
+++ frontend/src/app/db/usuarioMock.ts
@@ -1,6 +1,6 @@
 import { Usuario } from "../model/usuario";
 
 export const UsuarioList: Usuario[] = [
-    new Usuario(1, "Henrique", "henrique.ciraulo@hotmail.com", "123", "41409247805", "123"),
-    new Usuario(2, "Esthefani", "esthefani.alves@yahoo.com.br", "123", "41409247805", "123")
+    new Usuario(1, "Henrique", "henrique.ciraulo@hotmail.com", "123", "1234", "123"),
+    new Usuario(2, "Esthefani", "esthefani.alves@yahoo.com.br", "123", "1234", "123")
 ];
